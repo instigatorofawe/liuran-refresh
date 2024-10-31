106fecfa2266f8bc5c7de5a39dd2a9aeabf7690b
Ran Liu <xabsox@gmail.com>
2024-10-31 16:48:01 -0700

    Unit test wasm module

--- src/routes/software/+page.svelte
+++ src/routes/software/+page.svelte
@@ -51,6 +51,12 @@ import { goto } from '$app/navigation'
     margin: 0.75em 0;
 }
 
+.description {
+    font-size: 12pt;
+    font-weight: 300;
+    margin: 0.75em 0;
+}
+
 .clickable:hover {
     cursor: pointer;
 }
@@ -61,19 +67,28 @@ import { goto } from '$app/navigation'
 
 <div class="software-item">
 <a href="https://github.com/mh105/somata"><h2>SOMATA: State-space oscillator modeling and time series analysis</h2></a>
-<h3>State-space Oscillator Modeling And Time-series Analysis (SOMATA) is a Python library for state-space neural signal
-processing algorithms developed in the Purdon Lab.</h3>
+<div class="description">State-space Oscillator Modeling And Time-series Analysis (SOMATA) is a Python library for state-space neural signal
+processing algorithms developed in the Purdon Lab.</div>
 <h3>Authors: Mingjian He, Proloy Das, Ran Liu, Amanda Beck, Patrick Purdon</h3>
 </div>
 
 <h1>Toy projects</h1>
 
+<div class="software-item clickable" onclick={goto("/software/sudoku")}>
+<h2>Sudoku Solver</h2>
+<div class="description">
+A Sudoku solver, written in Rust and compiled to WebAssembly. Empty cells with only one possible solution are filled in
+first, before resorting to depth-first search and backtracking, starting with the cell with the fewest possible
+candidates.
+</div>
+</div>
+
 <div class="software-item clickable" onclick={goto("/software/tictactoe")}>
 <h2>Tic-Tac-Toe Solver</h2>
-<h3>
+<div class="description">
 A Tic-Tac-Toe solver. The game is solved via tabular minimax, with reflection and rotation-invariant hashes computed in
 order to reduce the number of game states to 765.
-</h3>
+</div>
 </div>
 
 </div>
